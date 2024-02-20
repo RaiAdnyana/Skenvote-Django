5b182f934b553c95dbb04c12e6e90a8ba84922bb
Rai Adnyana <142960649+RaiAdnyana@users.noreply.github.com>
2024-02-20 19:19:54 +0800

    Update urls.py

--- project/login/urls.py
+++ project/login/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
-from .views import user_login, signup, submit, home, voting, hasil, about
+from .views import login, signup, submit, home, voting, hasil, about, BerhasilVoting
 
 
 urlpatterns = [
-    path('', user_login, name='login'),
+    path('', home, name='home'),
     path('signup/', signup, name='signup'),
     path('submit/', submit, name='submit'),
-    path('home/', home, name='home'),
+    path('login/', login, name='login'),
     path('voting/', voting, name='voting'),
     path('hasil/', hasil, name='hasil'),
     path('about/', about, name='about'),
+    path('BerhasilVoting/', BerhasilVoting, name='BerhasilVoting'),
 ]
