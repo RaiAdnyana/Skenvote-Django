6699403d5b99bd300604193bb232640cb82a3a9f
Rai Adnyana <142960649+RaiAdnyana@users.noreply.github.com>
2024-02-20 19:22:21 +0800

    Update settings.py

--- project/project/settings.py
+++ project/project/settings.py
@@ -117,9 +117,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL="/media/"
+MEDIA_ROOT=os.path.join(BASE_DIR,"media/")
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 APPEND_SLASH = False
+
+
+
+
+
 
