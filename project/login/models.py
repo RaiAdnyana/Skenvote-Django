df5c115e2dd0f5d80ca50e09ee4f6295204f8e26
Rai Adnyana <142960649+RaiAdnyana@users.noreply.github.com>
2024-02-20 19:19:25 +0800

    Update models.py

--- project/login/models.py
+++ project/login/models.py
@@ -1,30 +1,23 @@
 from django.db import models
-from django.contrib.auth.backends import BaseBackend
-
     
-class Login (models.Model):
-    username = models.CharField(max_length=200, null=False, blank=False)
-    password = models.CharField(max_length=200, null=False, blank=False)
 
+class Image(models.Model):
+    caption=models.CharField(max_length=30)
+    image=models.ImageField(upload_to="img/%y")
+    def str(self):
+        return self.caption
+
+
+class Gambar(models.Model):
+    deskripsi=models.CharField(max_length=30)
+    foto=models.ImageField(upload_to="img/%y")
+    def str(self):
+        return self.deskripsi
+    
+class Login(models.Model):
+    username=models.CharField(max_length=20)
+    password=models.CharField(max_length=20)
     def __str__(self):
-       return self.username
-    
-    
-class Signup(models.Model):
-    username2 = models.CharField(max_length=100, null=False, blank=False)
-    email = models.EmailField(unique=True, null=False, blank=False)
-    password2 = models.CharField(max_length=100, null=False, blank=False)
-
-    def __str__(self):
-        return self.username2
-    
-class Kandidat(models.Model):
-    foto1 = models.ImageField(upload_to='login/static/img', null=False, blank=False)
-    nama_kandidat1 = models.CharField(max_length=100)
-    foto2 = models.ImageField(upload_to='login/static/img', null=False, blank=False)
-    nama_kandidat2 = models.CharField(max_length=100)
-
-    def __str__(self):
-        return f"{self.nama_kandidat1} {self.foto1} vs {self.nama_kandidat2} {self.foto2}"
+        return self.username
 
 
