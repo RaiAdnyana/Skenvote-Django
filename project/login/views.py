7344e118404f5d5d0cb12aa77a2aa54c499a0cf4
Rai Adnyana <142960649+RaiAdnyana@users.noreply.github.com>
2024-02-20 19:21:17 +0800

    Update views.py

--- project/login/views.py
+++ project/login/views.py
@@ -1,20 +1,22 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate
-from .utils import authenticate_signup
-from .models import Login
-from .models import Signup
-from .models import Kandidat
+from django.contrib.auth import login as auth_login, authenticate
+from .models import Image, Gambar, Login
+from django.contrib.auth.models import User
 
 
-def user_login(request):
+def login(request):
     if request.method == 'POST':
         username = request.POST.get('Username')
         password = request.POST.get('Password')
 
-        authenticated_user = authenticate_signup(username=username, password=password)
+        User= Login(username=username,password=password)
+        User.save()
 
-        if authenticated_user is not None:  # Periksa apakah autentikasi berhasil
-            login(request, authenticated_user)
+        # Autentikasi pengguna menggunakan backend autentikasi kustom
+        authenticated_user = authenticate(request, username=username, password=password)
+
+        if authenticated_user is not None:  
+            auth_login(request, authenticated_user)
             return redirect('home')
         else:
             return render(request, 'index.html', {'error_message': 'Authentication failed'})
@@ -22,9 +24,7 @@ def user_login(request):
     else:
         return render(request, 'index.html')
     
-    
 
-    
 
 def signup(request):
     if request.method == 'POST':
@@ -32,29 +32,22 @@ def signup(request):
         email = request.POST.get('Email2')
         password = request.POST.get('Password2')
 
-        signup_data = Signup.objects.create(username2=username, email=email, password2=password)
+        # Membuat pengguna baru
+        user = User.objects.create_user(username=username, email=email, password=password)
 
+        # Autentikasi pengguna
+        authenticated_signup = authenticate(request, username=username, password=password)
 
-        # Simpan user ke dalam database
-        try:
-            signup_data = Signup.objects.create(username2=username, email=email, password2=password)
-
-            # Autentikasi pengguna
-            authenticated_signup_data = authenticate(request, username=email, password=password)
-
-            if authenticated_signup_data is not None and authenticated_signup_data.is_authenticated:
-                # Jika autentikasi berhasil, signup dan arahkan ke halaman submit
-                login(request, authenticated_signup_data)
-                return redirect('submit')
-            else:
-                # Jika autentikasi gagal, tampilkan pesan kesalahan
-                return render(request, 'signup.html', {'error_message': 'Gagal melakukan pendaftaran'})
-            
-        except Exception as e:
-            return render(request, 'signup.html', {'error_message': f'Gagal melakukan pendaftaran: {e}'})
-
+        if authenticated_signup is not None:
+            # Jika autentikasi berhasil, lakukan login dan arahkan ke halaman beranda
+            auth_login(request, authenticated_signup)
+            return redirect('submit')
+        else:
+            # Jika autentikasi gagal, tampilkan pesan kesalahan
+            return render(request, 'signup.html', {'error_message': 'Autentikasi gagal'})
     else:
         return render(request, 'signup.html')
+
     
 
 def submit(request):
@@ -64,14 +57,22 @@ def home(request):
     return render(request, 'home.html')
 
 def voting(request):
-    # Mengambil semua objek Kandidat dari database
-    kandidat_list = Kandidat.objects.all()
+    image_pics = Image.objects.all()  # Query images from Image model
+    gambar_pics = Gambar.objects.all()  # Query images from Gambar model
 
-    # Mengirimkan data ke template menggunakan konteks
-    return render(request, 'voting.html', {'kandidat_list': kandidat_list})
+    # Pass both sets of objects to the template context using different variables
+    context = {
+        'image_pics': image_pics,
+        'gambar_pics': gambar_pics,
+    }
+    return render(request, 'voting.html', context)
 
 def hasil(request):
     return render(request, 'hasil.html')
 
 def about(request):
     return render(request, 'about.html')
+
+def BerhasilVoting(request):
+    return render(request, 'BerhasilVoting.html')
+
